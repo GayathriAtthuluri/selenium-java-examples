21937c47427eb5243853783a9b607fcee2f2af1b
GayathriAtthuluri <atthulurigayathri@gmail.com>
2026-07-08 18:34:24 +0530

    Add Selenium utility Examples

--- src/utilities/FileHandlingExamples.java
+++ src/utilities/FileHandlingExamples.java
@@ -1,10 +1,60 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class FileHandlingExamples {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    private static final String FILE_PATH = "C:\\Users\\gayatria\\Downloads\\TestData.xlsx";
 
-	}
+    // Read Excel Data
+    public void readExcelFile() throws IOException {
+
+        FileInputStream file = new FileInputStream(FILE_PATH);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+        XSSFSheet sheet = workbook.getSheet("LoginData");
+
+        int totalRows = sheet.getLastRowNum();
+
+        int totalColumns = sheet.getRow(0).getLastCellNum();
+
+        System.out.println("Total Rows : " + totalRows);
+
+        System.out.println("Total Columns : " + totalColumns);
+
+        System.out.println("\n===== Excel Data =====\n");
+
+        for (int i = 0; i <= totalRows; i++) {
+
+            for (int j = 0; j < totalColumns; j++) {
+
+                String cellValue = sheet.getRow(i).getCell(j).toString();
+
+                System.out.print(cellValue + "\t");
+
+            }
+
+            System.out.println();
+
+        }
+
+        workbook.close();
+
+        file.close();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        FileHandlingExamples example = new FileHandlingExamples();
+
+        example.readExcelFile();
+
+    }
 
 }
--- src/utilities/ScreenshotExamples.java
+++ src/utilities/ScreenshotExamples.java
@@ -1,8 +1,71 @@
 package utilities;
-class ScreenshotExamples
-{
-	public static void main(String[] args)
-	{
-		System.out.println("hi");
-	}
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScreenshotExamples {
+
+    private WebDriver driver;
+
+    private static final String URL = "https://www.saucedemo.com/";
+
+    // Launch Browser
+    public void launchApplication() {
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get(URL);
+
+        System.out.println("Application Launched Successfully");
+
+    }
+
+    // Capture Screenshot
+    public void captureScreenshot() throws IOException, InterruptedException {
+         File screenshotFile = ((TakesScreenshot) driver)
+                .getScreenshotAs(OutputType.FILE);
+
+        File destinationFile = new File("Screenshots/HomePage.png");
+
+        destinationFile.getParentFile().mkdirs();
+
+        FileUtils.copyFile(screenshotFile, destinationFile);
+        Thread.sleep(5000);
+        System.out.println(destinationFile.getAbsolutePath());
+        System.out.println("Screenshot Captured Successfully");
+
+    }
+
+    // Close Browser
+    public void closeBrowser() {
+
+        driver.quit();
+
+        System.out.println("Browser Closed Successfully");
+
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ScreenshotExamples example = new ScreenshotExamples();
+
+        example.launchApplication();
+
+        example.captureScreenshot();
+
+        example.closeBrowser();
+
+    }
+
 }
